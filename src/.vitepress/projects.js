37b053b36974aeb121d4306fe0704b4d020ba084
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-08-18 18:41:14 -0300

    update

--- src/.vitepress/projects.js
+++ src/.vitepress/projects.js
@@ -2,7 +2,7 @@ export default [
   {
     url: '/project/pali-wallet/',
     title: 'Pali Wallet',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    description: 'An Extension Wallet for Sys Blockchain.',
     stack: 'react, typescript, redux',
     link: 'https://chrome.google.com/webstore/detail/pali-wallet/mgffkfbidihjpoaomajlbgchddlicgpn?hl=pt-br&amp;authuser=0/',
     logo: '/assets/images/logo-m.png',
