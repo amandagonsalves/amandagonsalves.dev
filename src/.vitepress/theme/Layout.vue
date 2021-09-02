b82156d6597cb6b143474afb74812dbc923c2517
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-09-02 17:03:27 -0300

    fix

--- src/.vitepress/theme/Layout.vue
+++ src/.vitepress/theme/Layout.vue
@@ -2,7 +2,16 @@
   <div>
     <div
       v-if="state.menuOpen"
-      class="w-full h-full-h block md:hidden bg-brand-darktransparent fixed inset-y-0 z-10"
+      class="
+        w-full
+        h-full-h
+        block
+        md:hidden
+        bg-brand-darktransparent
+        fixed
+        inset-y-0
+        z-10
+      "
       @click="setState"
     ></div>
 
@@ -17,7 +26,7 @@
 </template>
 
 <script>
-import { computed } from "vue";
+import { computed, onMounted, onUnmounted, reactive, watch } from "vue";
 import { postForPath } from "../../utils/helpers";
 import { useRoute } from "vitepress/dist/client/app/router";
 import { state } from "./store";
@@ -32,18 +41,61 @@ export default {
     Title,
   },
   setup() {
-    const route = useRoute();
-    const project = computed(() => postForPath(route.path));
+    const resizeHandler = () => {
+      if (
+        window.innerWidth > 600 ||
+        document.documentElement.clientWidth > 600 ||
+        document.body.clientWidth > 600
+      ) {
+        state.menuOpen = false;
+        state.selectedImageId = -1;
+      }
+    };
+
+    onMounted(() => {
+      window.addEventListener("resize", resizeHandler);
+    })
+
+    onUnmounted(() => {
+      window.removeEventListener("resize", resizeHandler);
+    })
+
+    const { offsetWidth, offsetHeight } = document.querySelector("#app");
+
+    const windowObj = reactive({
+      innerWidth: window.innerWidth,
+      clientWidth: document.documentElement.clientWidth,
+      bodyClientWidth: document.body.clientWidth,
+    });
 
     const setState = () => {
       state.menuOpen = false;
       state.selectedImageId = -1;
-    }
+    };
+
+    watch(
+      () => windowObj,
+      (windowObj, prev) => {
+        console.log("window obj", windowObj, prev);
+        if (
+          window.innerWidth > 600 ||
+          document.documentElement.clientWidth > 600 ||
+          document.body.clientWidth > 600
+        ) {
+          setState();
+        }
+      }
+    );
+
+    const route = useRoute();
+    const project = computed(() => postForPath(route.path));
 
     return {
       project,
       state,
-      setState
+      setState,
+      offsetWidth,
+      offsetHeight,
     };
   },
 };
