a1a528c484f42d96ceb0117fddf01dae62114844
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-09-16 22:10:10 -0300

    update

--- src/about/about.vue
+++ src/about/about.vue
@@ -42,19 +42,34 @@
       </h2>
 
       <p class="mb-6 font-poppins">
-        I’m 18 years old and I’m passionate about solving problems and learning, especially new technologies, languages and the financial market.
+        I’m 18 years old and I’m passionate about solving problems and learning,
+        especially new technologies, languages and the financial market.
       </p>
 
       <p class="mb-6 font-poppins">
-        I work with React, Vue, TypeScript and CSS preprocessors like SCSS at frontend. At backend I have experience with Node and NoSQL (MongoDB)
+        I work with React, Vue, TypeScript and CSS preprocessors like SCSS at
+        frontend. At backend I have experience with Node and NoSQL (MongoDB)
       </p>
 
       <p class="mb-6 font-poppins">
-        I also have experience using blockchain technologies, such as Solidity, Web3.js, Truffle, dApps, Bitcoin and Ethereum’s blockchain doing some side projects to improve my knowledge and also working on a crypto wallet project for SYS blockchain.
+        I have experience working on top of the Ethereum ecosystem. Developing
+        from the protocol level (with solidity as the main language, always
+        using cutting edge tools to improve the process like truffles or
+        helmet).
       </p>
 
       <p class="mb-6 font-poppins">
-        I’m always open to new challenges and ready to learn everything I can to do a good job and be better every day.
+        At Dapp level, I use Web3.js with front-end frameworks. I also developed
+        an extension wallet (<a href="https://paliwallet.com/" target="_blank" >paliwallet.com</a>) along with the Syscoin blockchain
+        during this year, where I have been working on top of the bitcoin.js and
+        syscoinjs-lib.js libraries, which is deepening my knowledge of the
+        structure of blockchains itself and allowing me to build on top of any
+        bitcoin chain as well.
+      </p>
+
+      <p class="mb-6 font-poppins">
+        I’m always open to new challenges and ready to learn everything I can to
+        do a good job and be better every day.
       </p>
     </div>
   </div>
