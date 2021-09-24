88cd2c1718f74fd8bca41ce77466b040e29c4249
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-09-24 12:27:26 -0300

    update

--- src/about/about.vue
+++ src/about/about.vue
@@ -3,9 +3,7 @@
     class="
       w-full
       text-brand-dark
-      flex
-      md:flex-row
-      flex-col
+
       items-center
       bg-brand-primary
       h-auto
@@ -18,21 +16,20 @@
     "
     id="About"
   >
-    <div class="w-1/2 flex justify-center items-center">
+    <!-- <div class="w-1/2 flex justify-center items-center">
       <img
-        src="/assets/images/profile.jpeg"
+        src="/assets/images/kkkkk.png"
         alt="profile"
         class="rounded-full w-1/2"
       />
-    </div>
+    </div> -->
 
-    <div class="w-full md:w-1/2 px-8 md:px-0">
+    <div class="w-full md:w-full px-8 md:px-0 max-w-2xl text-center m-auto">
       <h2
         class="
           text-3xl
-          mb-6
+          mb-8
           font-merienda
-          md:text-left
           text-center
           mt-4
           md:mt-0
@@ -48,7 +45,7 @@
 
       <p class="mb-6 font-poppins">
         I work with React, Vue, TypeScript and CSS preprocessors like SCSS at
-        frontend. At backend I have experience with Node and NoSQL (MongoDB)
+        frontend. At backend I have experience with Node and NoSQL (MongoDB).
       </p>
 
       <p class="mb-6 font-poppins">
@@ -59,11 +56,11 @@
       </p>
 
       <p class="mb-6 font-poppins">
-        At Dapp level, I use Web3.js with front-end frameworks. I also developed
+        At dApp level, I use web3.js with front-end frameworks. I also developed
         an extension wallet (<a class="text-blue-800" href="https://paliwallet.com/" target="_blank" >paliwallet.com</a>) along with the Syscoin blockchain
-        during this year, where I have been working on top of the <a href="https://github.com/bitcoinjs/bitcoinjs-lib" target="_blank" class="text-blue-800">bitcoin.js</a> and
-        <a href="https://github.com/syscoin/syscoinjs-lib" class="text-blue-800" target="_blank">syscoinjs-lib.js</a> libraries, which is deepening my knowledge of the
-        structure of blockchains itself and allowing me to build on top of any
+        during this year, where I have been working on top of the <a href="https://github.com/bitcoinjs/bitcoinjs-lib" target="_blank" class="text-blue-800">bitcoinjs-lib</a> and
+        <a href="https://github.com/syscoin/syscoinjs-lib" class="text-blue-800" target="_blank">syscoinjs-lib</a> libraries, which is deepening my knowledge of the
+        structure of blockchain itself and allowing me to build on top of any
         Bitcoin chain as well.
       </p>
 
