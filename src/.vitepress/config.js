3ac19e090ed899a1a0bd8ae62eb70e0a794ecf6e
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-10-01 11:05:09 -0300

    update

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -32,23 +32,15 @@ module.exports = {
       'script',
       {
         async: true,
-        src: 'https://www.googletagmanager.com/gtag/js?id=G-HRTZ5KMKVJ',
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-YHCZ89BVCN',
       },
     ],
     [
       'script',
       {},
       [
-        "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-HRTZ5KMKVJ');",
+        "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-YHCZ89BVCN');",
       ],
     ],
   ],
-  plugins: [
-    [
-      '@vuepress/google-analytics',
-      {
-        'ga': 'UA-209049245-1' // UA-00000000-0
-      }
-    ]
-  ]
 }
