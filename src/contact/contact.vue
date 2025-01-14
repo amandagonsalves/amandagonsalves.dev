dc40b566e7c7cd9449bfa6fa15ab71ce1cb0d9d1
Amanda Gonsalves <amandagonsalvesags@gmail.com>
2025-01-14 12:24:28 -0300

    chore(contact): update order

--- src/contact/contact.vue
+++ src/contact/contact.vue
@@ -3,16 +3,15 @@
     class="w-full text-brand-dark flex flex-col justify-center items-center h-auto p-12 max-w-6xl mx-auto"
     id="Contact"
   >
-    <h2 class="text-3xl mb-6 font-merienda">Contact</h2>
+    <h2 class="text-3xl font-merienda">Contact</h2>
+    <div
+      class="w-full"
+      id="book-a-call"
+    ></div>
     <p class="mb-6 font-poppins">amandagonsalvesags@gmail.com</p>
 
     <Icons />
   </div>
-
-  <div
-    class="w-full flex flex-col justify-center items-center h-auto max-w-6xl mx-auto"
-    id="book-a-call"
-  ></div>
 </template>
 
 <script>
